38e5b597614d1f765372b5eace49d91800a437dd
Pelin Karakoca <karakocapelin@gmail.com>
2022-11-28 01:18:36 +0300

    Completed assignment

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  console.log(props);
+    if (!props.isPublished) {
+      return null;
+    }
+    else {
+      return (<div id="blog-content">
+      <h3>{props.articleText}</h3>
+      <p>{props.minutesToRead} minutes to read</p>
+     </div>
+     )
+    }
 }
 
 export default BlogContent;
